ba860005ccfbc47630747db0e7677d7624ddbcfe
Tomás <146328829+TomasSilvaGomes@users.noreply.github.com>
2025-12-06 13:23:53 +0000

    Update stage5.py

--- Scripts/stage5.py
+++ Scripts/stage5.py
@@ -81,7 +81,7 @@ class BasicBlockSE(nn.Module):
         out = self.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out = self.se(out)
-        out += self.shortcut(x)
+        out += self.shortcut(x) # Residual Connection
         out = self.relu(out)
         return out
 
